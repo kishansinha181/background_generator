64379bfc4ca1a4b91bcd8bbe11efd0cee37a396f
Kishan Sinha <kishan.sinha18@gmail.com>
2020-05-08 14:19:20 +0530

    Final

---
+++ script.js
@@ -0,0 +1,43 @@
+var css = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.getElementById("gradient");
+var button = document.getElementById("button");
+
+
+
+function randomGen() {
+	var x = Math.round(0xffffff * Math.random()).toString(16);
+	var y = (6 - x.length);
+	var z ="000000";
+	var z1 = z.substring(0, y);
+	return "#" + z1 + x;
+}
+
+function setGradient() {
+	body.style.background =
+		"linear-gradient(to right, "
+		+ color1.value
+		+ ", "
+		+ color2.value
+		+ ")";
+
+	css.textContent = body.style.background + ";";
+}
+
+color1.addEventListener("input", setGradient);
+
+color2.addEventListener("input", setGradient);
+
+button.addEventListener("click",function(){
+	body.style.background =
+		"linear-gradient(to right, "
+		+ randomGen()
+		+ ", "
+		+ randomGen()
+		+ ")";
+
+	css.textContent = body.style.background + ";";	
+
+
+})
